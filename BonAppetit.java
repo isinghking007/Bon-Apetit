dc0bf7847983635c8f3b6627cfcc4d6678e97f78
Avinash Kumar <61631621+isinghking007@users.noreply.github.com>
2020-09-12 07:08:33 +0530

    Add files via upload

---
+++ BonAppetit.java
@@ -0,0 +1,42 @@
+package hackerrank;
+import java.io.*;
+import java.util.*;
+public class BonAppetit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the size ");
+		int n=in.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter the various dishes they eat");
+		int i;
+		for(i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+		}
+		System.out.println("Enter the item that Anna didn't eat ");
+		int k=in.nextInt();
+		System.out.println("The amount charged by Anna");
+		int b=in.nextInt();
+		int actualsum=0,finalsum=0,sum=0;
+		for(i=0;i<n;i++)
+		{
+			actualsum=(actualsum+a[i]);
+			sum=actualsum/2;
+		}
+		System.out.println(sum);
+		finalsum=(actualsum-a[k])/2;
+		System.out.println("Final sum "+finalsum);
+		if(b==finalsum)
+		{
+			System.out.println("Bon Appetit");
+		}
+		else
+		{
+
+			System.out.println("difference is "+(sum-finalsum));
+		}
+	}
+
+}
